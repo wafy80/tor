15b519ee2dd15cfae549e7c0c3fd7b0a89da2fc4
Alfredo Galvagno <119013510+wafy80@users.noreply.github.com>
2026-03-27 10:26:40 +0100

    added privoxy

--- start-tor.sh
+++ start-tor.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Start Privoxy (in background, non blocking)
+privoxy /etc/privoxy/config &
+
 # Start Tor
 tor &
 
-# Get the PID of Tor
+# Get the PID of Tor (main service to monitor)
 TOR_PID=$!
 
 # Function to check the status of Tor
 check_tor() {
-    kill -0 "$TOR_PID" 2>/dev/null
-    return $?
+ kill -0 "$TOR_PID" 2>/dev/null
+ return $?
 }
 
 # Infinite loop to monitor Tor
 while true; do
-    if ! check_tor; then
-        echo "Tor is not running. Restarting..."
-        tor &
-        TOR_PID=$!
-    fi
-    sleep 10
+ if ! check_tor; then
+  echo "Tor is not running. Restarting..."
+  tor &
+  TOR_PID=$!
+ fi
+ sleep 10
 done
